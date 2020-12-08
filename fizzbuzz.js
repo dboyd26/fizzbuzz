d69f4a5910675a4d16a66ada90ee96c17834f62c
dboyd26 <73416547+dboyd26@users.noreply.github.com>
2020-12-08 14:08:39 -0600

    changes
    Going through

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,27 +1,27 @@
-let x = 1;
-for ( x=1; x <100; x++)
+let x = 0;
+for ( x=1; x <100; x++) // the loop begins at  1, count is to 100, x++ is the interation
 {
     
-if ([x] % 7 == 0 &&  [x] %  3 == 0)
+if ( x % 5 == 0 &&  x %  3 == 0) // if x divided by 5 has a remainder of 0 and  x divided by 3 has a remainder of 0, an action will occur
 {
     
-    document.querySelector("#displayAll").innerHTML  += "fizzbuzz<br>";
+    document.querySelector("#displayAll").innerHTML  += "fizzbuzz" + "<br>";// the action that will occur
 }
 
 else if (x % 5 == 0)
 {
  
- document.querySelector("#displayAll").innerHTML  += "buzz<br>"
+ document.querySelector("#displayAll").innerHTML  += "buzz" + "<br>";
 }
 else if (x % 3 == 0)
 {
  
- document.querySelector("#displayAll").innerHTML  += "fizz<br>";
+ document.querySelector("#displayAll").innerHTML  += "fizz" + "<br>";
 }
 
 else 
 {
     
-    document.querySelector("#displayAll").innerHTML  += x;
+document.querySelector("#displayAll").innerHTML  += x + "<br>";
 }
 }
